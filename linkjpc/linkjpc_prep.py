7d5235c7faf6a5ce69ee3242b2df6efa7b250497
nomotom <nomotom@gmail.com>
2021-12-05 01:33:55 +0900

    (linkedjson2tsv)a minor adjustment of comments

--- linkjpc/linkjpc_prep.py
+++ linkjpc/linkjpc_prep.py
@@ -1692,7 +1692,7 @@ def linkedjson2tsv(linked_json_dir, title2pid_org_file, log_info):
             sample
                 2392906	桐谷華	地位職業	声優	38	20	38	22	1192	声優
     notice
-        '\n' in text(mention) has been converted to '__NR__'.
+        '\n' in text(mention) has been converted to '\\n'.
     """
 
     import json
